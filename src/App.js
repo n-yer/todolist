8bd32bffbd70d1d93f75bdefe6870c6d90b6a523
n-yer <60143911+n-yer@users.noreply.github.com>
2020-03-11 17:10:10 +0200

    c

--- src/App.js
+++ src/App.js
@@ -9,16 +9,21 @@ class Task extends React.Component{
     super(props);
     this.state = {
       completed: 0,
+      style: "task",
+      deleteMe: 0
     }
   }
 
+  doSomething(e){
+    this.state.completed == 0 ? this.setState({completed: 1, style: "task complete-task",}) : this.setState({completed: 0, style: "task",})    
+  }
 
   render(){
     return(
-      <li className="task">
-        <input type="checkbox" className="task-checkbox" onClick={() => this.props.onClick()}/>
+      <li className={this.state.style} >
+        <input type="checkbox" className="task-checkbox" onClick={this.doSomething.bind(this)} />
         <p className="task-name">{this.props.title}</p>
-        <p>Completed: {this.props.completed}</p>
+        <button className="delete-btn" onClick={this.props.dispose}>X</button>
       </li>
       
     );
@@ -29,49 +34,58 @@ class ToDoList extends React.Component{
   constructor(props){
     super(props);
 
-    this.defaultTasks = ["Task 1", "Task 2", "Task 3", "Task 4"]
+    this.defaultTasks = ["Task 1"]
+    this.tasks = [];
+    for(var i=0; i< this.defaultTasks.length; i++){
+      this.tasks.push(this.createTask(this.defaultTasks[i]))
+    }
+    this.state = {
+      newTask : "",
+      removeTask: ""
+    }
     
   }
 
-  handleTaskClick(){
-    this.props.completed = 1;
+  createTask(title){
+    return <Task title={title} completed={0} dispose={this.deleteTask.bind(this)} />;
+  }
+  addNewTask(){
+    if(this.state.newTask != ""){
+      this.tasks.push(this.createTask(this.state.newTask))
+      this.setState({newTask: ""});
+    } 
   }
 
-  createTask(title){
-    return <Task title={title} completed={0} onClick={() => this.handleTaskClick()}/>;
+  deleteTask(e){
+    console.log(e)
+    for(var i = 0; i< this.tasks.length; i++){
+      if(this.tasks[i] == e.target.title)
+        this.tasks.splice(i, 1);
+    }
+    this.tasks.splice(this.tasks.indexOf(e.target.title), 1);
+    this.forceUpdate()
   }
   render(){
     return(
 
       <div className="main">
         <div className="task-list">
-          {
-            for(var i=0; i< this.defaultTasks.length; i++){
-              createTask(this.defaultTasks[i])
-            }
-          
-          }
+          {this.tasks}
         </div>
+        <input onChange={(e) => {this.setState({newTask: e.target.value})}} value={this.state.newTask}></input>
+        <button className="hero-button" onClick={this.addNewTask.bind(this)}>Add Task</button>
       </div>
-      
-      
     );
   }
 }
 function App() {
   
-  
-
   return (
     <div className="App">
       <Helmet>
         <meta charSet="utf-8" />
         <title>To Do List</title>
-        <link rel="icon" type="image/png" href="http://example.com/myicon.png" /> 
       </Helmet>
-
-
-
 
       <ToDoList/>
     </div>
