94db4f115c60838c5a9df85574544d7f9771e8bd
n-yer <60143911+n-yer@users.noreply.github.com>
2020-03-11 09:35:36 +0200

    c

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Helmet from 'react-helmet';
 
+
+class Task extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      completed: 0,
+    }
+  }
+
+
+  render(){
+    return(
+      <li className="task">
+        <input type="checkbox" className="task-checkbox" onClick={() => this.props.onClick()}/>
+        <p className="task-name">{this.props.title}</p>
+        <p>Completed: {this.props.completed}</p>
+      </li>
+      
+    );
+  }
+}
+
+class ToDoList extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.defaultTasks = ["Task 1", "Task 2", "Task 3", "Task 4"]
+    
+  }
+
+  handleTaskClick(){
+    this.props.completed = 1;
+  }
+
+  createTask(title){
+    return <Task title={title} completed={0} onClick={() => this.handleTaskClick()}/>;
+  }
+  render(){
+    return(
+
+      <div className="main">
+        <div className="task-list">
+          {
+            for(var i=0; i< this.defaultTasks.length; i++){
+              createTask(this.defaultTasks[i])
+            }
+          
+          }
+        </div>
+      </div>
+      
+      
+    );
+  }
+}
 function App() {
+  
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>To Do List</title>
+        <link rel="icon" type="image/png" href="http://example.com/myicon.png" /> 
+      </Helmet>
+
+
+
+
+      <ToDoList/>
     </div>
   );
 }
