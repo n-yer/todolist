be0c8755a015803b8afccf3dbc25be5e049df7e3
expir3 <>
2020-07-08 16:06:39 +0200

    Updates

---
+++ precache-manifest.dc54fb849344375612d89e08d4ced25b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fd345af88c9cdb7c5ee1c5e99e8abb0",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "2fde338c719ddb305a8d",
+    "url": "/todo/static/css/main.617c5932.chunk.css"
+  },
+  {
+    "revision": "6e4062211820788945bb",
+    "url": "/todo/static/js/2.b8d14254.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/todo/static/js/2.b8d14254.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fde338c719ddb305a8d",
+    "url": "/todo/static/js/main.870fbb2e.chunk.js"
+  },
+  {
+    "revision": "a65703e962574574e50d",
+    "url": "/todo/static/js/runtime-main.3669824a.js"
+  }
+]);
